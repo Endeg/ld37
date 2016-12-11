967fb6c23dd0eae282000d9a9ea333a321ea234c
Andrey Gladyshev <vodomerkin@gmail.com>
2016-12-11 21:15:58 +0600

    But with unpausing from message even in battle mode.

--- Message.gd
+++ Message.gd
@@ -2,17 +2,20 @@ extends Popup
 
 var messages = []
 var bgObject = null
+var active = false
 
 func _ready():
 	set_process_input(true)
 
 func showMessages(theMessages = []):
 	messages = theMessages
-	nextMessage()
+	if not messages.empty():
+		nextMessage()
 
 func nextMessage():
 	hide()
 	if not messages.empty():
+		active = true
 		get_tree().set_pause(true)
 		var msg = messages[0]
 		assert msg != null
@@ -20,10 +23,11 @@ func nextMessage():
 		get_node("Panel/RichTextLabel").set_bbcode(msg)
 		popup_centered_ratio()
 	else:
+		active = false
 		get_tree().set_pause(false)
 	
 func _input(event):
-	if event.is_action_released("player-use"):
+	if active and event.is_action_released("player-use"):
 		nextMessage()
 
 func setBackgroundObject(obj):
