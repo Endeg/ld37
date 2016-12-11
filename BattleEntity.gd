75bddb2228cb835a180b893233e6fe7aa0a43b6f
Andrey Gladyshev <vodomerkin@gmail.com>
2016-12-11 15:27:50 +0600

    Battle screen in progress,

--- BattleEntity.gd
+++ BattleEntity.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
 var animation = null
+var count = 0
 
 func _ready():
 	animation = get_node("AnimationPlayer")
 	assert animation != null
 	
+	#animation.play("Damage")
+
+func setEntityName(name = "noname"):
+	var lbl = get_node("Label")
+	assert lbl != null
+	lbl.set_text(name)
+
+func displayDamage():
 	animation.play("Damage")
+
+func displayAttack():
+	animation.play("Attack")
+ 
